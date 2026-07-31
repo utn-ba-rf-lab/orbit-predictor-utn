a6bb23651543831f25f02ff92f4212b3aa2bfb86
omar-sto <7297886+omar-sto@users.noreply.github.com>
2026-07-31 16:44:18 -0300

    Feature: Agregado de modos Desarrollo (reducir delay) y Debug (level del logger)

--- tracker.py
+++ tracker.py
@@ -8,15 +8,27 @@ import subprocess
 import logging
 import sys
 from custom_classes import CustomPredictor, CustomOverpass
+import argparse
 
-# Configurar el logger para que escriba a stdout (al journalctl).
-logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(message)s')
+# Configuración de argumentos
+parser = argparse.ArgumentParser()
+parser.add_argument("--dev", action="store_true", dest="DEV", help="Ejecutar en modo desarrollo - Reduce el Delay entre pasadas")
+parser.add_argument("--debug", action="store_true", dest="DEBUG", help="Mostrar información detallada de depuración en los logs")
+ARGS = parser.parse_args()
+
+# Configurar el logger para que escriba a stdout (al journalctl) según el modo (Level INFO por defecto)
+if ARGS.DEBUG:
+    logging.basicConfig(level=logging.DEBUG, format='%(levelname)s - %(message)s')
+else:
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(message)s')
 
 logger = logging.getLogger(__name__)
 
 MAX_AWAITABLE_PASSES = 5
 LAUNCH_BEFORE_SECS = dt.timedelta(seconds=10)
 
+#Factor de reducción de delay para Modo Desarrollo (--dev)
+DEV_DELAY_FACTOR=0.0001
 
 def sorted_by_aos(_list):
     return sorted(_list, key=lambda p: p.aos)
@@ -62,6 +74,11 @@ def filter_overlapping_passes(passes, track_list):
 
 async def pass_worker_async(p, track):
     delay = (p.aos - dt.datetime.now(dt.timezone.utc) - LAUNCH_BEFORE_SECS).total_seconds()
+
+    # Reducción de delay en Modo Desarrollo (--dev)
+    if ARGS.DEV:
+        delay = delay * DEV_DELAY_FACTOR
+
     await asyncio.sleep(max(0, delay))
 
     cmd = [
